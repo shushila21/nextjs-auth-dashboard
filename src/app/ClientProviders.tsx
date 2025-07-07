b55c5e6fe0ab406ba928db10317101cac888b7ba
Shushila Budhathoki <shushila.budhathoki28@gmail.com>
2025-07-07 23:41:27 +0545

    feat: create `ClientProviders` for adding React Query provider & SessionProvider

---
+++ src/app/ClientProviders.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConfigProvider } from "antd";
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+
+const queryClient = new QueryClient();
+
+export default function ClientProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <ConfigProvider>{children}</ConfigProvider>
+      </QueryClientProvider>
+    </SessionProvider>
+  );
+}
